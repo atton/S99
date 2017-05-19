d375c8fee80201d8c446c7195b0cfe63e12cb538
atton <e115763@gmail.com>
2017-05-19 12:29:28 +0900

    Refactor S01

--- src/main/scala/S01.scala
+++ src/main/scala/S01.scala
@@ -4,8 +4,9 @@ package main.scala
   * Created by yasutaka_higa on 2017/05/19.
   */
 object S01 {
-  def last(list : List[Int]) : Int = {
-    val len = list.length
-    if (len == 0) 0 else list(len-1)
+  def last(list : List[Int]) : Int = list match {
+    case (x :: Nil) => x
+    case (x :: xs)  => last(xs)
+    case _          => throw new RuntimeException("empty list")
   }
 }
