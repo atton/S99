f65f4bc0a6b0cb177097bca6607bbc07b1d34f4d
atton <e115763@gmail.com>
2017-05-22 14:09:17 +0900

    Replace S?? -> P??

--- src/test/scala/S99Spec.scala
+++ src/test/scala/S99Spec.scala
@@ -8,15 +8,15 @@ import main.scala._
 
 class S99Spec extends FlatSpec with DiagrammedAssertions with MockitoSugar {
 
-  "S01.last" should "return last element from list" in {
+  "P01.last" should "return last element from list" in {
     assert(8 === P01.last(List(1, 1, 2, 3, 5, 8)))
   }
 
-  "S02.penultimate" should "return last but one element of a list" in {
+  "P02.penultimate" should "return last but one element of a list" in {
     assert(5 === P02.penultimate(List(1, 1, 2, 3, 5, 8)))
   }
 
-  "S03.nth" should "return Kth element of a list." in {
+  "P03.nth" should "return Kth element of a list." in {
     assert(2 === P03.nth(2, List(1, 1, 2, 3, 5, 8)))
 
     intercept[Exception](P03.nth(0, List()))
@@ -25,72 +25,72 @@ class S99Spec extends FlatSpec with DiagrammedAssertions with MockitoSugar {
     intercept[Exception](P03.nth(-1, List(0)))
   }
 
-  "S04.length" should "return number of elements of list" in {
+  "P04.length" should "return number of elements of list" in {
     assert(6 === P04.length(List(1, 1, 2, 3, 5, 8)))
   }
 
-  "S05.reverse" should "reverse a list" in {
+  "P05.reverse" should "reverse a list" in {
     assert(P05.reverse(List(1, 1, 2, 3, 5, 8)) === List(8, 5, 3, 2, 1, 1))
   }
 
-  "S06.isParindrome" should "return whether a list is a palindrome" in {
+  "P06.isParindrome" should "return whether a list is a palindrome" in {
     assert(true === P06.isPalindrome(List(1, 2, 3, 2, 1)))
     assert(true === P06.isPalindrome(List(1, 2, 2, 1)))
     assert(false === P06.isPalindrome(List(1, 2, 3, 2, 6)))
 
   }
 
-  "S07.flatten" should "return flatten a nested list structure" in {
+  "P07.flatten" should "return flatten a nested list structure" in {
     assert(P07.flatten(List(List(1, 1), 2, List(3, List(5, 8)))) === List(1, 1, 2, 3, 5, 8))
   }
 
-  "S08.compress" should "eliminate consecutive duplicates of list elements" in {
+  "P08.compress" should "eliminate consecutive duplicates of list elements" in {
     assert(P08.compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
       ===  List('a, 'b, 'c, 'a, 'd, 'e))
   }
 
-  "S09.pack" should "pack consecutive duplicates of list elements into sublists" in {
+  "P09.pack" should "pack consecutive duplicates of list elements into sublists" in {
     assert(P09.pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
       === List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e)))
   }
 
-  "S10.encode" should "return Run-length encoding of a list" in {
+  "P10.encode" should "return Run-length encoding of a list" in {
     assert(P10.encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
       ===  List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e)))
   }
 
-  "S11.encodeModified" should "return Modified run-length encoding" in {
+  "P11.encodeModified" should "return Modified run-length encoding" in {
     assert(P11.encodeModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
       === List((4,'a), 'b, (2,'c), (2,'a), 'd, (4,'e)))
   }
 
-  "S12.decode" should "decode a run-length encoded list" in {
+  "P12.decode" should "decode a run-length encoded list" in {
     assert(P12.decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
       === List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
   }
 
-  "S13.encodeDirect" should "return Run-length encoding of a list (direct solution)" in {
+  "P13.encodeDirect" should "return Run-length encoding of a list (direct solution)" in {
     assert(P13.encodeDirect(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
       === List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e)))
   }
 
-  "S14.duplicate" should "duplicate the elements of a list" in {
+  "P14.duplicate" should "duplicate the elements of a list" in {
     assert(P14.duplicate(List('a, 'b, 'c, 'c, 'd)) === List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd))
   }
 
-  "S15.duplicateN" should "duplicate the elements of a list a given number of times" in {
+  "P15.duplicateN" should "duplicate the elements of a list a given number of times" in {
     assert(P15.duplicateN(3, List('a, 'b, 'c, 'c, 'd)) === List('a, 'a, 'a, 'b, 'b, 'b, 'c, 'c, 'c, 'c, 'c, 'c, 'd, 'd, 'd))
   }
 
-  "s16.drop" should "drop every Nth element from a list." in {
+  "P16.drop" should "drop every Nth element from a list." in {
     assert(P16.drop(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) === List('a, 'b, 'd, 'e, 'g, 'h, 'j, 'k))
   }
 
-  "s17.split" should "split a list into two parts." in {
+  "P17.split" should "split a list into two parts." in {
     assert(P17.split(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) === (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k)))
   }
 
-  "s18.slice" should "extract a slice from a list." in {
+  "P18.slice" should "extract a slice from a list." in {
     assert(P18.slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) === List('d, 'e, 'f, 'g))
   }
 
